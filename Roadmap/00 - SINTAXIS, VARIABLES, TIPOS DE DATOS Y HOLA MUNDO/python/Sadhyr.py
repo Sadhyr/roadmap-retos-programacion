466881daceae4cd896310ee2de0509be0a24f4a5
Sadhyr <dgn.clau@gmail.com>
2024-08-29 09:46:27 -0500

    Create Sadhyr.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/Sadhyr.py
@@ -0,0 +1,18 @@
+# https://python.org
+
+# Comentario para una linea
+
+"""
+Comentario para varias
+lineas
+"""
+
+'''
+Otro comentario en varia
+lineas
+'''
+
+my_variable = "mi variable"
+my_variable = "mi nueva variable"
+
+  
